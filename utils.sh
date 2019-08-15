9045c3df30efaab1d73d891ff0803f3105b28f1c
Wang Wei <wangwei1237@gmail.com>
2019-08-15 16:44:44 +0800

    fixed some bugs

--- utils.sh
+++ utils.sh
@@ -7,11 +7,18 @@ PYTHON='/usr/local/bin/python'
 FFPROBE='/usr/local/bin/ffprobe'
 ADB='/Users/wangwei/Library/Android/sdk/platform-tools/adb'
 BIN='/Users/wangwei/Documents/Project/github/video_codec_evaluation/get_frame_seq'
+
+# 因为并不是所有的应用保存的路径都一致，所以要分别处理
 VIDEO_PATH='/sdcard/相机'
-VIDEO_AFTER_PATH='videoAfter'
-VIDEO_DB_PATH='videoDB'
+VIDEO_PATH_2='/sdcard/DCIM/Camera'
+
+# ADB模拟点击的坐标均基于OWxOH的设备分辨率获取，因此需要针对不同机型进行缩放.
 OW=1080
 OH=1920
+
+# 视频素材库路径和存放处理后视频的路径
+VIDEO_DB_PATH='videoDB'
+VIDEO_AFTER_PATH='videoAfter'
 
 . appInfo.sh
 
@@ -74,12 +81,21 @@ function cleanDevice() {
 
 	local files=$($ADB shell ls $VIDEO_PATH)
 	
-	echo "...清空设备上所有的视频..."
+	echo "...清空设备上${VIDEO_PATH}目录下的所有的视频..."
 	for file in $files
 	do
 		echo "......删除${file}"
 		$ADB shell rm -f "${VIDEO_PATH}/${file}"
 		$ADB shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d "file://${VIDEO_PATH}/${file}"
+	done
+
+	echo "...清空设备上${VIDEO_PATH_2}目录下的所有的视频..."
+	files=$($ADB shell ls $VIDEO_PATH_2)
+	for file in $files
+	do
+		echo "......删除${file}"
+		$ADB shell rm -f "${VIDEO_PATH_2}/${file}"
+		$ADB shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d "file://${VIDEO_PATH_2}/${file}"
 	done
 
 	return 0
@@ -108,13 +124,18 @@ function pushVideo2Device() {
 }
 
 ###
-# 获取应用处理后，保存到本地的视频，该视频需要拉到本地处理. 
+# @brief: 获取应用处理后，保存到本地的视频，该视频需要拉到本地处理. 
+#
+# @param path:string, 查找path目录下是否已经生成文件.
+#
+# @echo file:string, 输出需要处理的文件名
 function getFileToBePull() {
-	local file=$($ADB shell ls $VIDEO_PATH | grep -v 'anchor_')
+	local path=$1
+	local file=$($ADB shell ls $path | grep -v 'anchor_')
 	while [ -z "$file" ]
 	do
 		sleep 2
-		file=$($ADB shell ls $VIDEO_PATH | grep -v 'anchor_')
+		file=$($ADB shell ls $path | grep -v 'anchor_')
 	done
 
 	echo $file
@@ -148,31 +169,31 @@ handleDouYin() {
 	x=$(echo "scale=0; 540/$rw" | bc)
 	y=$(echo "scale=0; 1840/$rh" | bc)
 	$ADB shell input tap $x $y
-	sleep 1
+	sleep 5
 
 	# 4. 点击上传，进入相册选择页面
 	x=$(echo "scale=0; 890/$rw" | bc)
 	y=$(echo "scale=0; 1650/$rh" | bc)
 	$ADB shell input tap $x $y 
-	sleep 1
+	sleep 5
 
 	# 5. 选择视频
 	x=$(echo "scale=0; 130/$rw" | bc)
 	y=$(echo "scale=0; 500/$rh" | bc)
 	$ADB shell input tap $x $y 
-	sleep 1
+	sleep 5
 
 	# 6. 点击<下一步>
 	x=$(echo "scale=0; 920/$rw" | bc)
 	y=$(echo "scale=0; 220/$rh" | bc)
 	$ADB shell input tap $x $y 
-	sleep 15
+	sleep 5
 
 	# 7. 点击<下一步>
 	x=$(echo "scale=0; 920/$rw" | bc)
 	y=$(echo "scale=0; 1760/$rh" | bc)
 	$ADB shell input tap $x $y
-	sleep 15
+	sleep 5
 
 	# 8. 点击<发布>
 	x=$(echo "scale=0; 800/$rw" | bc)
@@ -188,7 +209,7 @@ handleDouYin() {
 
 	# 10. 获取文件
 	echo "......上传视频成功，获取保存到本地的视频..."
-	local file=$(getFileToBePull)
+	local file=$(getFileToBePull $VIDEO_PATH)
 
 	# 11. 将文件拉到指定的目录
 	echo ".........将视频拉取到电脑，准备分析..."
@@ -229,19 +250,19 @@ handleMini() {
 	x=$(echo "scale=0; 540/$rw" | bc)
 	y=$(echo "scale=0; 1840/$rh" | bc)
 	$ADB shell input tap $x $y
-	sleep 1
+	sleep 5
 
 	# 4. 点击上传，进入相册选择页面
 	x=$(echo "scale=0; 890/$rw" | bc)
 	y=$(echo "scale=0; 1650/$rh" | bc)
 	$ADB shell input tap $x $y 
-	sleep 1
+	sleep 5
 
 	# 5. 选择视频
 	x=$(echo "scale=0; 130/$rw" | bc)
 	y=$(echo "scale=0; 500/$rh" | bc)
 	$ADB shell input tap $x $y 
-	sleep 1
+	sleep 5
 
 	# 6. 点击<✔️>
 	x=$(echo "scale=0; 967/$rw" | bc)
@@ -263,17 +284,17 @@ handleMini() {
 
 	# 9. 获取文件
 	echo "......上传视频成功，获取保存到本地的视频..."
-	local file=$(getFileToBePull)
+	local file=$(getFileToBePull $VIDEO_PATH_2)
 
 	# 10. 将文件拉到指定的目录
 	echo ".........将视频拉取到电脑，准备分析..."
-	$ADB pull "${VIDEO_PATH}/${file}" "${VIDEO_AFTER_PATH}/mini_android_${handleFileName}"
+	$ADB pull "${VIDEO_PATH_2}/${file}" "${VIDEO_AFTER_PATH}/mini_android_${handleFileName}"
 	sleep 1
 
 	# 11. 删除手机的上文件并发送广播通知
 	echo "............拉取成功后，删除手机上的视频..."
-	$ADB shell rm -f "${VIDEO_PATH}/${file}"
-	$ADB shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d "file://${VIDEO_PATH}/${file}"
+	$ADB shell rm -f "${VIDEO_PATH_2}/${file}"
+	$ADB shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d "file://${VIDEO_PATH_2}/${file}"
 }
 
 ###
@@ -287,6 +308,7 @@ function analyseFrameDrop() {
 	echo "...开始分析丢帧的情况..."
 	for file in $files
 	do
+		echo "...分析文件${file}...BEGIN........."
 		tmp=${file%%.*}
 		originalVideoFile="${VIDEO_DB_PATH}/${file##*_android_}"
 		
@@ -310,7 +332,7 @@ rm -rf $VIDEO_AFTER_PATH/*
 file='videoDB/anchor_video_20190814_190000.mp4'
 pushVideo2Device $file
 sleep 2
-#handleDouYin $file
+handleDouYin $file
 handleMini $file
 sleep 2
 analyseFrameDrop

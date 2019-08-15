4853897b4f40b48a3d6f1e79f9fbe8e2c6a93e59
Wang Wei <wangwei1237@gmail.com>
2019-08-15 13:52:48 +0800

    增加分辨率缩放的功能，适配所有屏幕的分辨率点击

--- utils.sh
+++ utils.sh
@@ -10,6 +10,10 @@ BIN='/Users/wangwei/Documents/Project/github/video_codec_evaluation/get_frame_se
 VIDEO_PATH='/sdcard/相机'
 VIDEO_AFTER_PATH='videoAfter'
 VIDEO_DB_PATH='videoDB'
+OW=1080
+OH=1920
+
+. appInfo.sh
 
 ###
 #@brief: 获取视频的宽，高，帧数.
@@ -31,7 +35,7 @@ function isDeviceConnection() {
 	
 	if [ -z "$devices" ]
 	then
-		echo "没有挂载Android设备，请挂载..."
+		echo "...没有挂载Android设备，请挂载..."
 		return 0
 	fi
 
@@ -70,10 +74,10 @@ function cleanDevice() {
 
 	local files=$($ADB shell ls $VIDEO_PATH)
 	
-	echo "情况设备上所有的视频..."
+	echo "...清空设备上所有的视频..."
 	for file in $files
 	do
-		echo "删除${file}"
+		echo "......删除${file}"
 		$ADB shell rm -f "${VIDEO_PATH}/${file}"
 		$ADB shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d "file://${VIDEO_PATH}/${file}"
 	done
@@ -95,7 +99,7 @@ function pushVideo2Device() {
 	local file=$1
 	local filename=${file##*/}
 
-	echo "将视频文件${filename}推送到手机相册下"
+	echo "...将视频文件${filename}推送到手机相册下"
 
 	$ADB push $file "${VIDEO_PATH}/"
 	$ADB shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d "file://${VIDEO_PATH}/${filename}"
@@ -124,52 +128,150 @@ handleDouYin() {
 	local handleFileName=$1
 	handleFileName=${handleFileName##*/}
 
+	local resolution=$(getDeviceResolution)
+	resolution=($resolution)
+	local w=${resolution[0]}
+	local h=${resolution[1]}
+	local rw=$(echo "scale=2; $OW/$w" | bc)
+	local rh=$(echo "scale=2; $OH/$h" | bc)
+	local x=0
+	local y=0
+
 	# 1. 打开抖音
-	echo "打开抖音，开始上传视频..."
-	$ADB shell am start -n com.ss.android.ugc.aweme/.main.MainActivity
+	echo "...打开抖音，开始上传视频..."
+	$ADB shell am start -n "$DOUYIN"
 
 	# 2. 等待5秒，方便打开主界面
 	sleep 5
 
 	# 3. 点击+,进入视频拍摄页面
-	$ADB shell input tap 540 1840
+	x=$(echo "scale=0; 540/$rw" | bc)
+	y=$(echo "scale=0; 1840/$rh" | bc)
+	$ADB shell input tap $x $y
 	sleep 1
 
 	# 4. 点击上传，进入相册选择页面
-	$ADB shell input tap 890 1650
+	x=$(echo "scale=0; 890/$rw" | bc)
+	y=$(echo "scale=0; 1650/$rh" | bc)
+	$ADB shell input tap $x $y 
 	sleep 1
 
 	# 5. 选择视频
-	$ADB shell input tap 130 500
+	x=$(echo "scale=0; 130/$rw" | bc)
+	y=$(echo "scale=0; 500/$rh" | bc)
+	$ADB shell input tap $x $y 
 	sleep 1
 
 	# 6. 点击<下一步>
-	$ADB shell input tap 920 220
+	x=$(echo "scale=0; 920/$rw" | bc)
+	y=$(echo "scale=0; 220/$rh" | bc)
+	$ADB shell input tap $x $y 
 	sleep 15
 
 	# 7. 点击<下一步>
-	$ADB shell input tap 920 1760
+	x=$(echo "scale=0; 920/$rw" | bc)
+	y=$(echo "scale=0; 1760/$rh" | bc)
+	$ADB shell input tap $x $y
 	sleep 15
 
 	# 8. 点击<发布>
-	$ADB shell input tap 800 1800
+	x=$(echo "scale=0; 800/$rw" | bc)
+	y=$(echo "scale=0; 1800/$rh" | bc)
+	$ADB shell input tap $x $y 
 	sleep 5
 
 	# 9. 有同步到头条的选项就点击取消
-	$ADB shell input tap 540 1540
+	x=$(echo "scale=0; 540/$rw" | bc)
+	y=$(echo "scale=0; 1540/$rh" | bc)
+	$ADB shell input tap $x $y 
 	sleep 1
 
 	# 10. 获取文件
-	echo "上传视频成功，获取保存到本地的视频..."
+	echo "......上传视频成功，获取保存到本地的视频..."
 	local file=$(getFileToBePull)
 
 	# 11. 将文件拉到指定的目录
-	echo "将视频拉取到电脑，准备分析..."
+	echo ".........将视频拉取到电脑，准备分析..."
 	$ADB pull "${VIDEO_PATH}/${file}" "${VIDEO_AFTER_PATH}/douyin_android_${handleFileName}"
 	sleep 1
 
 	# 12. 删除手机的上文件并发送广播通知
-	echo "拉取成功后，删除手机上的视频..."
+	echo "............拉取成功后，删除手机上的视频..."
+	$ADB shell rm -f "${VIDEO_PATH}/${file}"
+	$ADB shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d "file://${VIDEO_PATH}/${file}"
+}
+
+### 
+# @brief: 将该文件上传到抖音
+# 
+# @param file:string: 此次处理的视频的文件名 
+handleMini() {
+	local handleFileName=$1
+	handleFileName=${handleFileName##*/}
+
+	local resolution=$(getDeviceResolution)
+	resolution=($resolution)
+	local w=${resolution[0]}
+	local h=${resolution[1]}
+	local rw=$(echo "scale=2; $OW/$w" | bc)
+	local rh=$(echo "scale=2; $OH/$h" | bc)
+	local x=0
+	local y=0
+
+	# 1. 打开全民小视频
+	echo "...打开全民小视频，开始上传视频..."
+	$ADB shell am start -n "$MINI"
+
+	# 2. 等待5秒，方便打开主界面
+	sleep 5
+
+	# 3. 点击+,进入视频拍摄页面
+	x=$(echo "scale=0; 540/$rw" | bc)
+	y=$(echo "scale=0; 1840/$rh" | bc)
+	$ADB shell input tap $x $y
+	sleep 1
+
+	# 4. 点击上传，进入相册选择页面
+	x=$(echo "scale=0; 890/$rw" | bc)
+	y=$(echo "scale=0; 1650/$rh" | bc)
+	$ADB shell input tap $x $y 
+	sleep 1
+
+	# 5. 选择视频
+	x=$(echo "scale=0; 130/$rw" | bc)
+	y=$(echo "scale=0; 500/$rh" | bc)
+	$ADB shell input tap $x $y 
+	sleep 1
+
+	# 6. 点击<✔️>
+	x=$(echo "scale=0; 967/$rw" | bc)
+	y=$(echo "scale=0; 1832/$rh" | bc)
+	$ADB shell input tap $x $y 
+	sleep 5
+
+	# 7. 点击<下一步>
+	x=$(echo "scale=0; 680/$rw" | bc)
+	y=$(echo "scale=0; 1760/$rh" | bc)
+	$ADB shell input tap $x $y 
+	sleep 5
+
+	# 8. 点击<发布>
+	x=$(echo "scale=0; 680/$rw" | bc)
+	y=$(echo "scale=0; 1730/$rh" | bc)
+	$ADB shell input tap $x $y
+	sleep 5
+
+	# 9. 获取文件
+	echo "......上传视频成功，获取保存到本地的视频..."
+	local file=$(getFileToBePull)
+
+	# 10. 将文件拉到指定的目录
+	echo ".........将视频拉取到电脑，准备分析..."
+	$ADB pull "${VIDEO_PATH}/${file}" "${VIDEO_AFTER_PATH}/mini_android_${handleFileName}"
+	sleep 1
+
+	# 11. 删除手机的上文件并发送广播通知
+	echo "............拉取成功后，删除手机上的视频..."
 	$ADB shell rm -f "${VIDEO_PATH}/${file}"
 	$ADB shell am broadcast -a android.intent.action.MEDIA_SCANNER_SCAN_FILE -d "file://${VIDEO_PATH}/${file}"
 }
@@ -182,19 +284,19 @@ function analyseFrameDrop() {
 	local originalVideoFile=""
 	local videoInfo=""
 
-	echo "开始分析丢帧的情况..."
+	echo "...开始分析丢帧的情况..."
 	for file in $files
 	do
 		tmp=${file%%.*}
-		originalVideoFile="${VIDEO_DB_PATH}/${file##douyin_android_}"
+		originalVideoFile="${VIDEO_DB_PATH}/${file##*_android_}"
 		
-		echo $originalVideoFile
-		echo $tmp
+		echo "......$originalVideoFile"
+		echo "......$tmp"
 		
-		echo "分析视频信息，得到分辨率和帧数..."
+		echo ".........分析视频信息，得到分辨率和帧数..."
 		videoInfo=$(getVideoInfo $originalVideoFile)
 		videoInfo=($videoInfo)
-		echo $videoInfo
+		echo ${videoInfo[@]}
 		
 		mkdir $tmp
 		$BIN -i "$VIDEO_AFTER_PATH/$file" -w ${videoInfo[0]} -h ${videoInfo[1]} -d $tmp
@@ -204,10 +306,12 @@ function analyseFrameDrop() {
 	done
 }
 
+rm -rf $VIDEO_AFTER_PATH/*
 file='videoDB/anchor_video_20190814_190000.mp4'
 pushVideo2Device $file
 sleep 2
-handleDouYin $file
+#handleDouYin $file
+handleMini $file
 sleep 2
 analyseFrameDrop
 sleep 2

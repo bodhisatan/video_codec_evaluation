7f3f72f6f591547b5da2ea18c4160dbf6be77947
Wang Wei <wangwei1237@gmail.com>
2019-11-17 15:34:41 +0800

    增加给视频打帧号的脚本

---
+++ videoLabelProcess.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#####################################################################
+# brief: 从手机拉取一个视频，然后对该视频执行预处理，再将视频推送到手机
+#####################################################################
+
+### 如下是会用到的配置项，使用时需要根据具体的命令进行替换。
+FFMPEG='/usr/local/bin/ffmpeg'
+FFPROBE='/usr/local/bin/ffprobe'
+
+if [ $# -ne 1 ]
+then
+	echo "参数错误，请检查..."
+	echo "Usage: $0 /fullpath/videofile"
+	exit 1
+fi
+
+videofile=$1
+filename=${videofile##*/}
+output="anchor_${filename}"
+
+video_bitrate=$($FFPROBE -v error -select_streams v:0  -show_entries stream=bit_rate -of default=nokey=1:noprint_wrappers=1 ${videofile})
+
+echo "开始给视频每一帧打上帧号..."
+vf="drawbox=40:190:160:60:Green:fill, drawtext=fontcolor=white:fontsize=50:fontfile=/Library/Fonts/Arial\ Bold.ttf:text=%{n}:x=50:y=200"
+$FFMPEG -noautorotate -i ${videofile} -metadata:s:v rotate=0 -b:v ${video_bitrate} -vf "${vf}" -y ${output}
+echo "给视频增加帧号处理完毕..."

59adab026a192bd7479c0d9c8a430f72806c04eb
Wang Wei <wangwei1237@gmail.com>
2019-11-27 17:03:15 +0800

    升级功能名称，讲MSE改为SSD，因为计算的时候实际上计算的是两个像素之间的差的平方，因为使用ssd来表述更为合适。

--- src/psnr.cpp
+++ src/psnr.cpp
@@ -34,21 +34,21 @@ double computeImagesMse(const unsigned char *main_data, const unsigned char *ref
     return mse / (double) (deno);
 }
 
-uchar mseNormal(int mse, int min_mse, int max_mse, int level, int normal) {
-    int oriInterval = (max_mse - min_mse + 1) / (int)level;
+uchar ssdNormal(int ssd, int min_ssd, int max_ssd, int level, int normal) {
+    int oriInterval = (max_ssd - min_ssd + 1) / (int)level;
     int normalInterval = (normal + 1) / (int)level;
 
     std::vector<int> oriList;
     std::vector<int> normalList;
 
     for (int i = 0; i <= level; ++i) {
-        oriList.emplace_back(min_mse + oriInterval * i);
+        oriList.emplace_back(min_ssd + oriInterval * i);
         normalList.emplace_back(normalInterval * i);
     }
     
     int res = 0;
     for (int i = 1; i <= level; ++i) {
-        if (mse <= oriList[i]) {
+        if (ssd <= oriList[i]) {
             res = normalList[i - 1];
             break;
         } 
@@ -57,15 +57,15 @@ uchar mseNormal(int mse, int min_mse, int max_mse, int level, int normal) {
     return (uchar)res;
 }
 
-void computeMseImage(const unsigned char *main_data, const unsigned char *ref_data, 
+void computeSsdImage(const unsigned char *main_data, const unsigned char *ref_data, 
     const EPixFormat format, const int w, const int h, const int plane, int frame_no, cv::VideoWriter &writer) {
     
     int size    = 0;
     int begin   = 0;
     int end     = 0;
     
-    int min_mse = 0;
-    int max_mse = 0;
+    int min_ssd = 0;
+    int max_ssd = 0;
 
     int w1 = 0;
     int h1 = 0;
@@ -76,25 +76,25 @@ void computeMseImage(const unsigned char *main_data, const unsigned char *ref_da
     if (plane == 1) {begin = w * h; end = (w * h) + ((w * h) >> 2); w1 = w >> 1; h1 = h >> 1;}
     if (plane == 2) {begin = (w * h) + ((w * h) >> 2); end = size; w1 = w >> 1; h1 = h >> 1;}
     
-    int mse = 0;
+    int ssd = 0;
 
-    min_mse = max_mse = pow2((int)main_data[begin] - (int)ref_data[begin]);
+    min_ssd = max_ssd = pow2((int)main_data[begin] - (int)ref_data[begin]);
 
     for (int i = begin + 1; i < end; ++i) {
-        mse = pow2((int)main_data[i] - (int)ref_data[i]);
-        if (mse < min_mse) {min_mse = mse;}
-        if (mse > max_mse) {max_mse = mse;}
+        ssd = pow2((int)main_data[i] - (int)ref_data[i]);
+        if (ssd < min_ssd) {min_ssd = ssd;}
+        if (ssd > max_ssd) {max_ssd = ssd;}
     }
     
-    if (max_mse > 255) {max_mse = 255;}
+    if (max_ssd > 255) {max_ssd = 255;}
 
     cv::Mat greyFrame = cv::Mat(h1, w1, CV_8UC1, cv::Scalar(255));
 
     for (int i = begin; i < end; ++i) {
-        mse = pow2((int)main_data[i] - (int)ref_data[i]);
-        uchar mse_nor = mseNormal(mse, min_mse, max_mse, 8, 255);
+        ssd = pow2((int)main_data[i] - (int)ref_data[i]);
+        uchar ssd_nor = ssdNormal(ssd, min_ssd, max_ssd, 8, 255);
         int j = i - begin;
-        greyFrame.at<uchar>((int)(j / w1), (int)(j % w1)) = mse_nor;
+        greyFrame.at<uchar>((int)(j / w1), (int)(j % w1)) = ssd_nor;
     }
 
     writer.write(greyFrame);
@@ -243,9 +243,9 @@ bool psnrAndVisualize(const std::string &main_video, const std::string &ref_vide
                   << " psnr_v:"   << getPsnr(mse_v, 255)  
                   << std::endl;
 
-        computeMseImage(b1, b2, YUV420, width, height, 0, current_frame, writer_y);
-        computeMseImage(b1, b2, YUV420, width, height, 1, current_frame, writer_u);
-        computeMseImage(b1, b2, YUV420, width, height, 2, current_frame, writer_v);  
+        computeSsdImage(b1, b2, YUV420, width, height, 0, current_frame, writer_y);
+        computeSsdImage(b1, b2, YUV420, width, height, 1, current_frame, writer_u);
+        computeSsdImage(b1, b2, YUV420, width, height, 2, current_frame, writer_v);  
 
         std::cout << "\r\033[k"; // 清空命令行.      
     }
